3cdb33138ab5515379c43577eba01d0e2f3f4dc2
Athlon1600 <Athlon2400@gmail.com>
2019-08-12 20:17:59 -0500

    misc formatting. we use intelij now

--- plugins/UrlFormPlugin.php
+++ plugins/UrlFormPlugin.php
@@ -1,39 +1,38 @@
 <?php
 
-use Proxy\Plugin\AbstractPlugin;
 use Proxy\Event\ProxyEvent;
+use Proxy\Plugin\AbstractPlugin;
 
-class UrlFormPlugin extends AbstractPlugin {
+class UrlFormPlugin extends AbstractPlugin
+{
+    public function onCompleted(ProxyEvent $event)
+    {
 
-	public function onCompleted(ProxyEvent $event){
-	
-		$request = $event['request'];
-		$response = $event['response'];
-		
-		$url = $request->getUri();
-		
-		// we attach url_form only if this is a html response
-		if(!is_html($response->headers->get('content-type'))){
-			return;
-		}
-		
-		// this path would be relative to index.php that included it?
-		$url_form = render_template("./templates/url_form.php", array(
-			'url' => $url
-		));
-		
-		$output = $response->getContent();
-		
-		// does the html page contain <body> tag, if so insert our form right after <body> tag starts
-		$output = preg_replace('@<body.*?>@is', '$0'.PHP_EOL.$url_form, $output, 1, $count);
-		
-		// <body> tag was not found, just put the form at the top of the page
-		if($count == 0){
-			$output = $url_form.$output;
-		}
-		
-		$response->setContent($output);
-	}
+        $request = $event['request'];
+        $response = $event['response'];
+
+        $url = $request->getUri();
+
+        // we attach url_form only if this is a html response
+        if (!is_html($response->headers->get('content-type'))) {
+            return;
+        }
+
+        // this path would be relative to index.php that included it?
+        $url_form = render_template("./templates/url_form.php", array(
+            'url' => $url
+        ));
+
+        $output = $response->getContent();
+
+        // does the html page contain <body> tag, if so insert our form right after <body> tag starts
+        $output = preg_replace('@<body.*?>@is', '$0' . PHP_EOL . $url_form, $output, 1, $count);
+
+        // <body> tag was not found, just put the form at the top of the page
+        if ($count == 0) {
+            $output = $url_form . $output;
+        }
+
+        $response->setContent($output);
+    }
 }
-
-?>
